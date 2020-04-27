64e136cf9fba3320a0e7816bd6a5ffcbc2a57ebd
ShadyFox2004 <amboudreau.amb@gmail.com>
2020-04-27 12:20:29 -0400

    add the greet func

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-#include <common.h>	//	include all the dependecy
-
+#include "common.h"	//	include all the dependecy
+#define USERNAME "ShadyFox"
 int main()
 {
-
+	printf("\tHello %s\t\n", USERNAME);
 	//	put your code to run here
 	return 1;
 }
