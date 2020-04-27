35101fc33f2ac028442567e0c3ef3ad4eb889227
ShadyFox2004 <amboudreau.amb@gmail.com>
2020-04-27 12:06:03 -0400

    add clearBuffer Fonction

---
+++ io.h
@@ -0,0 +1,10 @@
+/*	Librairie header file for io.c
+ *
+ *	contain fonction declaration
+ *
+ *	created by ShadyFox2004
+ */
+
+int clearBuffer ();	//	clear the stdin buffer to avoid unwanted entry 
+int userInput (char*, int);	//	get input from user and formatit for a string
+
